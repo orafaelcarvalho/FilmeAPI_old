c243ecb6e4213d2fb0a23a83435183ac2ca0ea71
Rafael Souza de Carvalho <rafael.carvalho@voiter.com>
2022-11-26 21:14:06 -0300

    Estudos

---
+++ FilmeAPI/FilmeAPI/Controllers/FilmeController.cs
@@ -0,0 +1,41 @@
+﻿using FilmeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmeAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int Id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = Id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme != null)
+                return Ok(filme);
+            return NotFound();
+        }
+    }
+}
---
+++ FilmeAPI/FilmeAPI/Data/FilmeContext.cs
@@ -0,0 +1,15 @@
+﻿using FilmeAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace FilmeAPI.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opt) : base(opt)
+        {
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
